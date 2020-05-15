1e4d6eda7fdd5f834be4963e38d793295d059b78
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-05-15 22:00:29 +0800

    [CMLG-007] INSTANCE-SEARCHIING Issues: Trying to make the code more understandable Solution: Rename the file, change the code in App.js, delete the useless code in index.css. Risk-Area: -- Reviewed by: --.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import StickyHeadTable from './DataTable';
 
+import MUIDataTable from "./TranslationTable";
 
-const useStyles = makeStyles(theme => ({
-  textfield: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch"
-    }
-  }
-}));
 
 export default function App() {
   return (
       <div className="App">
-          <SearchTextField />
-          <StickyHeadTable />
+         <MUIDataTable/>
       </div>
-  );
-}
-
-function SearchTextField() {
-  const classes = useStyles();
-
-  return (
-      <form className={classes.textfield} noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Standard" />
-      </form>
   );
 }
