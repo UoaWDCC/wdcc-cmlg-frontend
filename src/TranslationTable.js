ac178ca969bd7c236bb68caafe3b24c278452ca1
jge385 <49629647+jge385@users.noreply.github.com>
2020-06-14 12:01:49 +1200

    [CMLG-007] Display optional languages (#10)
    Issues: 
    This website is currently using dummy data, but we need to display actual data to users.
    
    Solutions:
    Manually copying the JSON file returned by backend and save it as the response.json file. Displaying this data on the website, searching functionality is purely done using functions from the frontend. 
    
    Risks: 
    Once the data is updated, the website does not renew synchronously.
    
    Review by: 
    Yujia, Emily, Annie, Eileen, Dave

--- src/TranslationTable.js
+++ src/TranslationTable.js
@@ -1,60 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 import MUIDataTable from "mui-datatables";
 /*This website is really useful: https://github.com/gregnb/mui-datatables*/
+
 import "./App.scss"
 
-const columns = ["Chinese", "English", "Korean", "Japanese", "Thailand"];
+import jsonData from './response.json';
 
-const data = [
-    ["Gabby George", "Business Analyst", "Minneapolis", 30, "$100,000"],
-    ["Aiden Lloyd", "Business Consultant", "Dallas", 55, "$200,000"],
-    ["Jaden Collins", "Attorney", "Santa Ana", 27, "$500,000"],
-    ["Franky Rees", "Business Analyst", "St. Petersburg", 22, "$50,000"],
-    ["Aaren Rose", "Business Consultant", "Toledo", 28, "$75,000"],
-    [
-        "Blake Duncan",
-        "Business Management Analyst",
-        "San Diego",
-        65,
-        "$94,000"
-    ],
-    ["Frankie Parry", "Agency Legal Counsel", "Jacksonville", 71, "$210,000"],
-    ["Lane Wilson", "Commercial Specialist", "Omaha", 19, "$65,000"],
-    ["Robin Duncan", "Business Analyst", "Los Angeles", 20, "$77,000"],
-    ["Mel Brooks", "Business Consultant", "Oklahoma City", 37, "$135,000"],
-    ["Harper White", "Attorney", "Pittsburgh", 52, "$420,000"],
-    ["Kris Humphrey", "Agency Legal Counsel", "Laredo", 30, "$150,000"],
-    ["Frankie Long", "Industrial Analyst", "Austin", 31, "$170,000"],
-    ["Brynn Robbins", "Business Analyst", "Norfolk", 22, "$90,000"],
-    ["Justice Mann", "Business Consultant", "Chicago", 24, "$133,000"],
-    [
-        "Addison Navarro",
-        "Business Management Analyst",
-        "New York",
-        50,
-        "$295,000"
-    ],
-    ["Jesse Welch", "Agency Legal Counsel", "Seattle", 28, "$200,000"],
-    ["Eli Mejia", "Commercial Specialist", "Long Beach", 65, "$400,000"],
-    ["Gene Leblanc", "Industrial Analyst", "Hartford", 34, "$110,000"],
-    ["Danny Leon", "Computer Scientist", "Newark", 60, "$220,000"],
-    ["Lane Lee", "Corporate Counselor", "Cincinnati", 52, "$180,000"],
-    ["Jesse Hall", "Business Analyst", "Baltimore", 44, "$99,000"],
-    ["Danni Hudson", "Agency Legal Counsel", "Tampa", 37, "$90,000"],
-    ["Terry Macdonald", "Commercial Specialist", "Miami", 39, "$140,000"],
-    ["Justice Mccarthy", "Attorney", "Tucson", 26, "$330,000"],
-    ["Silver Carey", "Computer Scientist", "Memphis", 47, "$250,000"],
-    ["Franky Miles", "Industrial Analyst", "Buffalo", 49, "$190,000"],
-    ["Glen Nixon", "Corporate Counselor", "Arlington", 44, "$80,000"],
-    [
-        "Gabby Strickland",
-        "Business Process Consultant",
-        "Scottsdale",
-        26,
-        "$45,000"
-    ],
-    ["Mason Ray", "Computer Scientist", "San Francisco", 39, "$142,000"]
-];
+
+const columns = ["zh_cn","English", "it_italiano", "arabic","serbian","croatian","russian","de_german","hebrew","fr_french","hu_hungarian","slovak","es_spanish","portugues","turkce","gr_greek","romanian"];
+
+var data = [];
+
+
+// get data from json file and input it to the table
+
+var nameArray=[];
+
+// load data from JSON file
+jsonData.map((x) =>{
+    return(
+            nameArray.push(x.name)
+        )
+    })
+
+
+// Input data to the table
+for(var i=0;i<nameArray.length;i+=0){
+    var output=[];
+    var j=0;
+    while(j<18){
+        output.push(nameArray[i]);
+        i++
+        j++;
+    }
+    data.push([output[0] + " " + output[1],output[2], output[3],output[4],output[5],output[6],output[7],output[8],output[9],output[10],output[11],output[12],output[13],output[14],output[15],output[16],output[17]]);
+
+}
 
 const options = {
     filterType: "dropdown",
@@ -67,6 +48,7 @@ const options = {
     filter:false,
     selectableRows: 'none',
     rowHover: false
+
 };
 
 
@@ -79,4 +61,3 @@ export default () => (
         options={options}
     />
 )
-
