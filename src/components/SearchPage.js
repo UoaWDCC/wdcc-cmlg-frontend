9b459777ecb3b57cbf4885710e68b56e96bf3b19
zyan225 <52368549+zyan225@users.noreply.github.com>
2020-07-11 19:06:00 +1200

    [CMLG-011] clean the file structure (#11)
    Issue:
    
    All files are written under the src folder, it is hard to find a specific file quickly.
    
    Solution:
    
    Creating a component folder to hold all react components created. The search page is created to render Table, SearchBar and SelectColumn components (yet to be implemented).
    A bootstrap CSS link is also added to allow the use of the bootstrap library in frontend development.
    
    Risk:
    No risk.
    Some files, e.g. TranslationTable.js, are not deleted at this point in case developers need to refer to them. These should be deleted in the future.
    
    Reviewed by:
    Yujia, Kevin, Annie, Eileen, Dave

---
+++ src/components/SearchPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class SearchPage extends React.Component {
+    constructor( props ) {
+        super( props );
+
+        this.state = {
+            //@todo place to store value such as search term and select column
+        }
+    }
+
+    render() {
+       return (
+            <div className="SearchPage">
+                place to render other components
+            </div>
+        );
+    }
+
+
+}
+
+export default SearchPage;
