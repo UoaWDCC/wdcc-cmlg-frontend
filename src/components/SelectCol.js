d9dfc4a7587344be6a0c5a7cc0645ca02003dfd9
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-07-12 12:09:42 +0800

    [CLMG-013] Select column Issue: Try to adjust the style of the form Solution: Using bootstrap component Risk: No risk. Reviewed by:--

--- src/components/SelectCol.js
+++ src/components/SelectCol.js
@@ -2,29 +2,27 @@ import React from 'react';
 
 class SelectCol extends React.Component {
     render() {
-
-        const { classes, columns, options } = this.props;
         return (
             <div className="custom-control custom-checkbox">
-                <div>
-                    <input type="checkbox" id="chinese" name="chinese" value="chinese"></input>
-                    <label for="check1">Chinese</label>
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="chinese"></input>
+                    <label class="custom-control-label" for="chinese">Chinese</label>
                 </div>
-                <div>
-                    <input type="checkbox" id="Japanese" name="Japanese" value="Japanese"></input>
-                    <label htmlFor="check1">Japanese</label>
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="english"></input>
+                    <label className="custom-control-label" for="english">English</label>
                 </div>
-                <div>
-                    <input type="checkbox" id="English" name="English" value="English"></input>
-                    <label htmlFor="check1">English</label>
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="japanese"></input>
+                    <label className="custom-control-label" for="japanese">Japanese</label>
                 </div>
-                <div>
-                    <input type="checkbox" id="French" name="French" value="French"></input>
-                    <label htmlFor="check1">French</label>
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="french"></input>
+                    <label className="custom-control-label" for="french">French</label>
                 </div>
-                <div>
-                    <input type="checkbox" id="German" name="German" value="German"></input>
-                    <label htmlFor="check1">German</label>
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="german"></input>
+                    <label className="custom-control-label" for="german">German</label>
                 </div>
             </div>
         );
