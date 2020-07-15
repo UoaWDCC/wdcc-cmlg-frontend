40b5577c7a947cfceb9a2931b3f1c5e9ed1fc271
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-07-15 15:28:37 +0800

    [CLMG-013] Select Column Issue: Try to add a border to the options. Solution: Using card component in Bootstrap. Risk: No risk Reviewed by:--

--- src/components/SelectCol.js
+++ src/components/SelectCol.js
@@ -3,31 +3,33 @@ import React from 'react';
 class SelectCol extends React.Component {
     render() {
         return (
-            <div className="custom-control custom-checkbox">
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" id="chinese"></input>
-                    <label class="custom-control-label" for="chinese">Chinese</label>
-                </div>
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" id="english"></input>
-                    <label className="custom-control-label" for="english">English</label>
-                </div>
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" id="japanese"></input>
-                    <label className="custom-control-label" for="japanese">Japanese</label>
-                </div>
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" id="french"></input>
-                    <label className="custom-control-label" for="french">French</label>
-                </div>
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" id="german"></input>
-                    <label className="custom-control-label" for="german">German</label>
+            <div className="card" style={{width:'10rem'}}>
+                <div className="card-body custom-control custom-checkbox">
+                    <h6 className="card-title">Show columns</h6>
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="chinese"></input>
+                            <label className="custom-control-label" htmlFor="chinese">Chinese</label>
+                        </div>
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="english"></input>
+                            <label className="custom-control-label" htmlFor="english">English</label>
+                        </div>
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="japanese"></input>
+                            <label className="custom-control-label" htmlFor="japanese">Japanese</label>
+                        </div>
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="french"></input>
+                            <label className="custom-control-label" htmlFor="french">French</label>
+                        </div>
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="german"></input>
+                            <label className="custom-control-label" htmlFor="german">German</label>
+                        </div>
                 </div>
             </div>
         );
     }
-
 }
 
 export default SelectCol;
