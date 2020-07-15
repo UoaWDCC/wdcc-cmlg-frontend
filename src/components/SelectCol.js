b0ed676c3f82601c35b1a67572667b01d26049a9
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-07-15 16:04:14 +0800

    [CMLG-013] Select Columns Issue: Try to toggle the card Solution: Using script and function Risk: May changed for  responsive test and position of the component need to adjust. Reviewd by:

--- src/components/SelectCol.js
+++ src/components/SelectCol.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class SelectCol extends React.Component {
+
+    openForm(){
+        const x = document.getElementById("language-options");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
     render() {
+        console.log( "The Select Col render function gets called" );
+
+        const cardStyle= {
+            width: "10rem",
+            display: "none",
+            position:"relative"
+        }
+
         return (
-            <div className="card" style={{width:'10rem'}}>
+            <div id="selectCol">
+            <button class="btn btn-primary" onClick={this.openForm}>Select Column</button>
+            <div id="language-options" className="card" style={cardStyle}>
                 <div className="card-body custom-control custom-checkbox">
                     <h6 className="card-title">Show columns</h6>
                         <div className="custom-control custom-checkbox">
@@ -27,6 +47,7 @@ class SelectCol extends React.Component {
                             <label className="custom-control-label" htmlFor="german">German</label>
                         </div>
                 </div>
+            </div>
             </div>
         );
     }
