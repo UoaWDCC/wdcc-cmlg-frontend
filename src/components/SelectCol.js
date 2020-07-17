95a1bfd0d176b344eda9ff1d7ba46f779abb4180
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-07-17 17:38:46 +0800

    [CMLG-013] Select Columns Combine two react components to one.

--- src/components/SelectCol.js
+++ src/components/SelectCol.js
@@ -1,57 +1,27 @@
 import React from 'react';
 
-class LanguagesList extends React.Component{
+class SelectCol extends React.Component {
     constructor(props) {
         super(props);
         this.getCheckedValues = this.getCheckedValues.bind(this);
-        this.state = {checkedValues:[]};
     }
 
-    getCheckedValues(){ // need to test if this function get the expected values
-       this.setState({checkedValues:[]});
-       const boxes = document.getElementsByClassName("custom-control-input");
+    getCheckedValues(){ //Get the selected Value and pass to other component
+        const values = [];
+
+        const boxes = document.getElementsByClassName("custom-control-input");
         for(var i=0;i<boxes.length;i++){
             if(boxes[i].checked){
-                this.state.checkedValues.push(boxes[i].id);
+                // this.state.checkedValues.push(boxes[i].id);
+                values.push(boxes[i].id);
             }
         }
         console.log("Checked Values:");
-        console.log(this.state.checkedValues);
+        console.log(values);
+        return values;
     }
 
-    render() {
-        const cardStyle= {
-            width: "10rem",
-            display: "none",
-            paddingLeft: "10px",
-            paddingBottom:"10px",
-            position:"fixed",
-            top: "45px",
-            right: "16px",
-            //overflow:"auto"
-        }
-
-        const languages = ["Chinese", "English", "Italian", "Arabic", "Serbian", "Croatian", "Russian", "German", "Hebrew", "French",
-            "Hungarian", "Slovak", "Spanish", "Portugues", "Turkce", "Greek", "Romanian"];
-
-        const listForm = languages.map((language) =>
-            <div className="custom-control custom-checkbox">
-                <input type="checkbox" className="custom-control-input" id={ language } onClick={() => this.getCheckedValues()}/>
-                <label className="custom-control-label" htmlFor={ language }>{ language }</label>
-            </div>
-        );
-
-        return(
-            <div id="language-options" className="card " style={ cardStyle }>
-                <h6 className="card-title" style={{color:"grey", paddingTop:"8px"}}>Show Columns</h6>
-                { listForm }
-            </div>
-        );
-    }
-}
-
-class SelectCol extends React.Component {
- /*   //need implement a parent call back function to pass the data to the table.
+    /*   //need implement a parent call back function to pass the data to the table.
     sendData = () => {
         this.props.parentCallback(this.state.data.checkedValues);
     },*/
@@ -87,6 +57,27 @@ class SelectCol extends React.Component {
             right: "16px"
         }
 
+        const cardStyle= {
+            width: "10rem",
+            display: "none",
+            paddingLeft: "10px",
+            paddingBottom:"10px",
+            position:"fixed",
+            top: "45px",
+            right: "16px",
+            //overflow:"auto"
+        }
+
+        const languages = ["Chinese", "English", "Italian", "Arabic", "Serbian", "Croatian", "Russian", "German", "Hebrew", "French",
+            "Hungarian", "Slovak", "Spanish", "Portugues", "Turkce", "Greek", "Romanian"];
+
+        const listForm = languages.map((language) =>
+            <div className="custom-control custom-checkbox">
+                <input type="checkbox" className="custom-control-input" id={ language } onClick={() => this.getCheckedValues()}/>
+                <label className="custom-control-label" htmlFor={ language }>{ language }</label>
+            </div>
+        );
+
         return (
             <div id="selectCol" onClick={ (e) => { this.handleClick(e) } }>
                 <button class="btn btn-light" onClick={ this.openForm } style={ buttonStyle } title="View Columns">
@@ -96,7 +87,10 @@ class SelectCol extends React.Component {
                               d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
                     </svg>
                 </button>
-                <LanguagesList/>
+                <div id="language-options" className="card " style={ cardStyle }>
+                    <h6 className="card-title" style={{color:"grey", paddingTop:"8px"}}>Show Columns</h6>
+                    { listForm }
+                </div>
             </div>
         );
     }
