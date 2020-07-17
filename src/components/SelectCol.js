4632aa5208acc2dda52a359ccaf5c2d647ed6a73
EileenSiqi <lsq0276665696@gmail.com>
2020-07-17 18:50:48 +1200

    [CMLG - 013] Select Columns Passing data
    Issue: The state of checkbox is not captured, we can't pass the data to table.
    
    Solution: Changing the state of the selected checkbox.
    
    Risk: Checkbox css format need to be revised.
    
    Reviewed by: Yujia

--- src/components/SelectCol.js
+++ src/components/SelectCol.js
@@ -1,45 +1,31 @@
 import React from 'react';
 
-class LanguagesList extends React.Component{
+class Language extends React.Component{
+
     render() {
-        const cardStyle= {
-            width: "10rem",
-            display: "none",
-            paddingLeft: "10px",
-            paddingBottom:"10px",
-            position:"fixed",
-            top: "45px",
-            right: "16px",
-            //overflow:"auto"
-        }
-
-        const languages = ["Chinese", "English", "Italian", "Arabic", "Serbian", "Croatian", "Russian", "German", "Hebrew", "French",
-            "Hungarian", "Slovak", "Spanish", "Portugues", "Turkce", "Greek", "Romanian"];
-
-        const listForm = languages.map((language) =>
-            <div className="custom-control custom-checkbox">
-                <input type="checkbox" className="custom-control-input" id={ language }/>
-                <label className="custom-control-label" htmlFor={ language }>{ language }</label>
-            </div>
-        );
-
         return(
-            <div id="language-options" className="card " style={ cardStyle }>
-                <h6 className="card-title" style={{color:"grey", paddingTop:"8px"}}>Show Columns</h6>
-                { listForm }
+            <div  className="custom-control custom-checkbox">
+                <input
+                    type="checkbox"
+                    checked={this.props.item.select}
+                    onChange={()=>{this.props.handleChange(this.props.item.id)}}
+                />
+                <label className="custom-control-label" htmlFor={ this.props.item.language }>{ this.props.item.language }</label>
+
             </div>
         );
+
     }
 }
 
 class SelectCol extends React.Component {
+
     constructor(props) {
         super(props);
-        /*this.state = {
-            data: {
-               　checkedValues: this.getCheckedValues()
-            }
-        }*/
+        this.state = {
+            selectLanguages: this.initLanguage()
+        }
+        this.handleChange = this.handleChange.bind(this)
     }
 
  /*   //need implement a parent call back function to pass the data to the table.
@@ -47,6 +33,16 @@ class SelectCol extends React.Component {
         this.props.parentCallback(this.state.data.checkedValues);
     },*/
 
+    initLanguage(){
+        const languages = ["Chinese", "English", "Italian", "Arabic", "Serbian", "Croatian", "Russian", "German", "Hebrew", "French",
+            "Hungarian", "Slovak", "Spanish", "Portugues", "Turkce", "Greek", "Romanian"]
+
+        let allLanguages = []
+        languages.forEach((language,index)=>
+            allLanguages.push({id:index,language:language,select:true}))
+        console.log(allLanguages)
+        return allLanguages
+    }
     //handle Event functions
     openForm(){
         const x = document.getElementById("language-options");
@@ -66,24 +62,26 @@ class SelectCol extends React.Component {
         e.nativeEvent.stopImmediatePropagation();
     }
 
-  /* getCheckedValues(){ // need to test if this function get the expected values
-        const boxes = document.getElementsByClassName("custom-control-input");
-        const boxesChecked = [];
-
-        for(var i=0;i<boxes.length;i++){
-            if(boxes[i].checked){
-                boxesChecked.push(boxes[i].id);
+    handleChange(id){
+        console.log("Changed:" + id)
+        this.setState(prevState => {
+            const updatedSelectLanguages = prevState.selectLanguages.map(selectLanguage => {
+                if (selectLanguage.id === id) {
+                    console.log("Changed:" + id)
+                    return{
+                        ...selectLanguage,
+                        select: !selectLanguage.select
+                    }
+                }
+                return selectLanguage
+            })
+            return {
+                selectLanguages: updatedSelectLanguages
             }
-        }
-
-        // Return the array if it is non-empty, or null
-        console.log("boxedChecked:"+ boxesChecked);
-        return boxesChecked.length > 0 ? boxesChecked : null;
-
-    }*/
+        })
+    }
 
     render() {
-        console.log( "The Select Col render function gets called" );
 
         //Style will move to css file later
         const buttonStyle ={
@@ -93,17 +91,34 @@ class SelectCol extends React.Component {
             top: "8px",
             right: "16px"
         }
+        const cardStyle= {
+            width: "10rem",
+            display: "none",
+            paddingLeft: "10px",
+            paddingBottom:"10px",
+            position:"fixed",
+            top: "45px",
+            right: "16px",
+            //overflow:"auto"
+        }
+
+        const Languages = this.state.selectLanguages.map(language => <Language key={language.id} item={language} handleChange={this.handleChange}/>)
+
+        console.log( "The Select Col render function gets called" );
 
         return (
             <div id="selectCol" onClick={ (e) => { this.handleClick(e) } }>
-                <button class="btn btn-light" onClick={ this.openForm } style={ buttonStyle } title="View Columns">
+                <button class="btn btn-light" onClick={ this.openForm } style={ buttonStyle } title="Select Language">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-list-check" fill="currentColor"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
                               d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
                     </svg>
                 </button>
-                <LanguagesList/>
+                <div id="language-options" className="card " style={ cardStyle }>
+                    <h6 className="card-title" style={{color:"grey", paddingTop:"8px"}}>Languages</h6>
+                    {Languages}
+                </div>
             </div>
         );
     }
