c2f41d7cd8015931ada87d1faddc3761b69b704c
jge385 <49629647+jge385@users.noreply.github.com>
2020-07-20 19:56:13 +1200

    [CMLG-012] Create search bar (#15)
     [CMLG-012] CreateSearchBar
     Issue: Need to pass the searching word to the search page
     Solution: Create a search bar
     Risk: Unknown
     Reviewed by: Any team member

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,24 +1,37 @@
 import React from 'react';
+import SearchBar from './SearchBar'
 import Table from "./Table";
 
 
 class SearchPage extends React.Component {
+
     constructor( props ) {
         super( props );
-
         this.state = {
-            //@todo place to store value such as search term and select column
-        }
+            word: ''
+        };
+    }
+
+    // change the searching word which is provided by the SearchBar class
+    handleChangeWord( searchWord ) {
+        this.setState( {
+            word: searchWord
+        } );
     }
 
     render() {
        return (
             <div className="SearchPage">
+                <SearchBar data = {
+                    {
+                        changeWord: this.handleChangeWord.bind(this)
+                    }
+                }> 
+                </SearchBar>   
                 <Table />
             </div>
         );
     }
-
 
 }
 
