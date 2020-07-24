64ec26df70bea267261210e1115cd77491eeed21
Yujia Wu <49899381+ywu666@users.noreply.github.com>
2020-07-24 14:25:29 +0800

    [CMLG-013] Select Columns
    Issues: We need a component to display all the languages and handle the change of user selection (i.e. select/unselect the language). 
    
    Solution: Initialize the state in SearchPage component and pass the state to SelectCol component to handle the change, then pass back to SearchPage.
    
    Risk: Unknown. Need to test by table component.
    
    Reviewed by: Yujia, Emily, Annie, Kavin, Dave

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import SelectCol from "./SelectCol";
 import SearchBar from './SearchBar'
 import Table from "./Table";
-
 
 class SearchPage extends React.Component {
 
     constructor( props ) {
         super( props );
         this.state = {
+            //@todo place to store value such as search term and select column
+            selectedColumns : this.initLanguages(),
             word: ''
         };
+    }
+
+    initLanguages() {
+        const languages = [ "Chinese", "English", "Italian", "Arabic", "Serbian", "Croatian", "Russian", "German", "Hebrew", "French",
+                            "Hungarian", "Slovak", "Spanish", "Portugues", "Turkce", "Greek", "Romanian" ]
+        let allLanguages = []
+        languages.forEach( ( language, index ) => {
+            if ( index < 5 ) {
+                allLanguages.push( {
+                    id: language,
+                    select: true
+                } )
+            }
+            else {
+                allLanguages.push( {
+                    id: language,
+                    select: false
+                } )
+            }
+        } )
+
+        return allLanguages;
     }
 
     // change the searching word which is provided by the SearchBar class
@@ -19,20 +43,22 @@ class SearchPage extends React.Component {
         } );
     }
 
+    // received all languages with their state from select Columns
+    handleSelectCol = ( allLanguages ) => {
+        this.setState( {
+            selectedColumns: allLanguages
+        } ) ;
+    }
+
     render() {
-       return (
-            <div className="SearchPage">
-                <SearchBar data = {
-                    {
-                        changeWord: this.handleChangeWord.bind(this)
-                    }
-                }> 
-                </SearchBar>   
+        return (
+            <div className = "SearchPage">
+                <SearchBar data = { { changeWord: this.handleChangeWord.bind( this ) } }> </SearchBar>
+                <SelectCol getsSelectedLanguage = { this.handleSelectCol } allLanguages = { this.state.selectedColumns }/>
                 <Table />
             </div>
         );
     }
-
 }
 
 export default SearchPage;
