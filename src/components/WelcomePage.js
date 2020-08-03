857931bffe4f6ae6cfdeba45e27bccece69e8b12
Annie Chau <annithinggoes@gmail.com>
2020-08-03 15:09:12 +1200

    [CMLG-001] Add button to welcome page
    Issue: No way to navigate from welcome page to the search page.
    
    Solution: Added button to welcome page that links to the search page.
    
    Risk: None
    
    Reviewed by: Yujia

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./css/WelcomePage.css";
+import { Link } from "react-router-dom";
 
 class WelcomePage extends React.Component {
     render() {
         return (
-          <p>hello welcome</p>
+          <div class="startButton">
+            <Link to="/translations">
+            <button type="button" class="btn btn-outline-dark">Start searching</button>
+            </Link>
+          </div>
         );
     }
 
