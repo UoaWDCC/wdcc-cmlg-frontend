08993550fed1e4d69f51be414777420d91a842ce
Annie Chau <annithinggoes@gmail.com>
2020-08-07 16:06:27 +1200

    [CMLG-001]  Made an image component for the background image
    Issue:
    Background image is not responsive
    
    Solution:
    Not complete, trying out another alternative having the image in an img tag in the html.
    
    Risk:--
    
    Reviewed by: Yujia

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./css/WelcomePage.css";
 import { Link } from "react-router-dom";
+import flagsImage from "./background.png";
 
 class WelcomePage extends React.Component {
     render() {
         return (
             <div className="welcomePage">
+                <img id="background-img" src={flagsImage} alt="Flags background"/>
                 <h1>Welcome</h1>
                 <p>to Crisis Translation</p>
                 <Link to="/translations">
