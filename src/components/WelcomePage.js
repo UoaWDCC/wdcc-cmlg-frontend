edfb1520f62bdbf35704095c78070fab079dc8d8
Annie Chau <annithinggoes@gmail.com>
2020-08-21 15:34:34 +1200

    [CMLG-001]  Centred the image again
    Issue:
    Image is still not centred on the page
    
    Solution:
    New image used. Adjusted code to match new width.
    
    Risk:--
    
    Reviewed by: Yujia, Emily, Eileen, Kevin, Dave

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./css/WelcomePage.css";
 import { Link } from "react-router-dom";
-import flagsImage from "./background3.png";
+import flagsImage from "./background4.png";
 
 class WelcomePage extends React.Component {
     render() {
