413491db479875fc20ef0b51ba97831c030d5cd8
zyan225 <52368549+zyan225@users.noreply.github.com>
2020-09-18 16:33:13 +1200

    [CMLG-015] Refactor the code in Table and SearchPage components (#38)
    Issue:
    
    To allow users to choose between 10 rows/all and which page to look at, a Toggle Buttons component and a Table Pagination component need to pass information to the Table. After getting the data from the backend, Table also needs to pass how many pages are needed to the Pagination components. There are lots of props being passed around, also Table and Pagination are both passing props to each other, which seems a bit messy.
    
    Solution:
    
    Move all API calling methods to the SearchPage component, the resulted data is passed to the Table as a prop.
    This leads to a new issue:
    
    The Table originally contains data as a state and changes this state when sorting data. However, it cannot modify its props.
    
    To solve that, the original sort function is split into two functions. Before displaying the data, a sort function is called to sort the data passed to the Table component based on the columnSortStatus state in Table. If user clicks a header, another function is called to change the columnSortStatus, which will trigger a re-rendering.
    
    After change above, ToggleButton and Pagination components will both change the state of SearchPage based on user's action. New information is passed to all components by the SearchPage component.
    
    Risk:
    /
    
    Reviewed by:
    Annie, Yujia, Kevin, Dave, Eileen

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,16 +5,20 @@ class Table extends React.Component {
     constructor( props ) {
         super( props )
         this.state = {
-            translationData: [],
-            columnSortStatus: new Array( 17 ).fill( "undefined" ),
-            loading: true, // True when the data is loading at initialisation. False when there are no search results.
-            sequence: -1
+            columnSortStatus: {
+                currentSortedColumnIndex: -1,
+                sortStatus: new Array( 17 ).fill( "undefined" )
+            }
         };
     }
 
     renderTableData() {
-        if ( this.state.translationData.length !== 0 ) {
-            return this.state.translationData.map( ( translation, index ) => {
+
+        if ( this.props.data.length !== 0 ) {
+
+            const sortedData = this.sortColumn();
+
+            return sortedData.map( ( translation, index ) => {
 
                 const [ chinese, pinyin, english ] = translation // Destructuring.
                 const translationsWithoutChineseAndEnglish = translation.slice( 3 );
@@ -25,7 +29,8 @@ class Table extends React.Component {
                             { chinese + " " + pinyin }
                         </td>
 
-                        {/* English is not put into the loop because we may need to put a special function
+                        {   // TODO fix the english column
+                            /* English is not put into the loop because we may need to put a special function
                             on it later to fix the position of this column, i.e. when the user scrolls the table
                             to the right, this column is fixed.
                          */}
@@ -53,7 +58,7 @@ class Table extends React.Component {
                     </tr>
                 )
             } )
-        } else if ( this.state.loading ) {
+        } else if ( this.props.isLoading ) {
             return (
                 <tr>
                     <td>{ "Loading" }</td>
@@ -70,13 +75,14 @@ class Table extends React.Component {
 
     renderTableHeaders() {
         return (
-            this.state.columnSortStatus.map( ( sortStatus, colIndex ) => {
+            this.state.columnSortStatus.sortStatus.map( ( sortStatus, colIndex ) => {
                 if ( colIndex === 1 ) {
                     return (
                         <th key={ colIndex } scope={ "col" } className={ sortStatus }
-                            onClick={ ( event ) => this.sortColumn( event ) }
+                            onClick={ ( event ) => this.onSortChanges( event ) }
                             style={ {
                                 display: this.props.columns[ colIndex ].select ? "table-cell" : "none",
+                                // TODO: fix this column when scrolling horizontally
                                 // left: this.props.columns[ 0 ].select ? "11.45%" : 0
                             } }>
                             { this.props.columns[ colIndex ].id }
@@ -85,7 +91,7 @@ class Table extends React.Component {
                 } else {
                     return (
                         <th key={ colIndex } scope={ "col" } className={ sortStatus }
-                            onClick={ ( event ) => this.sortColumn( event ) }
+                            onClick={ ( event ) => this.onSortChanges( event ) }
                             style={ {
                                 display: this.props.columns[ colIndex ].select ?
                                     "table-cell" : "none"
@@ -98,113 +104,65 @@ class Table extends React.Component {
         );
     }
 
-    getData() {
-        let sequenceTime = new Date();
-        let url = 'https://cmlgbackend.wdcc.co.nz/api/translations?sequence=' + sequenceTime.getTime() +
-                  '&word=' + this.props.words;
-
-        fetch( url )
-            .then( results => {
-                return results.json();
-            } )
-            .then( responseData => {
-                const data = responseData.data;
-                const sequence = responseData.sequence;
-
-                if ( sequence <= this.state.sequence ) {
-                    return;
-                }
-
-                let sortedListOfWords = [];
-                let translationsForOneWord = [];
-                let dataIndex;
-                let currentData;
-
-                for ( dataIndex = 0; dataIndex < data.length; dataIndex++ ) {
-                    currentData = data[ dataIndex ];
-
-                    // Check if the translated word is in the correct column (under the correct language). So if the
-                    // English translation for the word is not under English, throw an exception.
-                    // + 1 infront of translationsForOneWord.length because the index starts from 0, whereas language_id
-                    // starts from 1.
-                    if ( translationsForOneWord.length + 1 === currentData.language_id ) {
-                        translationsForOneWord[ translationsForOneWord.length ] = currentData.name;
-                    } else {
-                        throw new Error( "The translated word does not match the language." );
-                    }
-
-                    const numberOfLanguages = 18;
-                    // When the word is translated to all languages, add translationsForOneWord into sortedListOfWords.
-                    // Empty translationsForOneWord so a new translationsForOneWord can be made for a new word.
-                    if ( translationsForOneWord.length === numberOfLanguages ) {
-                        sortedListOfWords[ sortedListOfWords.length ] = translationsForOneWord;
-                        translationsForOneWord = [];
-                    }
-                }
-                this.setState( {
-                    translationData: sortedListOfWords,
-                    loading: false,
-                    sequence: sequence
-                } );
-            } )
-    }
-
-    componentDidMount() {
-        this.getData();
-    }
-
-    componentDidUpdate( prevProps, prevState, snapshot ) {
-        if ( this.props.words !== prevProps.words ) {
-            this.getData();
-        }
-    }
-
-    sortColumn( event ) {
-
-        // headers in the table are in the format: [ chinese + pinyin, english ... ]
-        // translationData contains array in the format: [ chinese, pinyin, english ... ]
+    onSortChanges( event ) {
         const clickedColumnIndex = event.target.cellIndex;
-        const sortElementIndex = clickedColumnIndex + 1;
 
-        let newColumnSortStatus = this.state.columnSortStatus.slice();
+        let newColumnSortStatusValue = this.state.columnSortStatus.sortStatus.slice();
 
-        newColumnSortStatus.map( ( sortDirection, colIndex ) => {
+        newColumnSortStatusValue.map( ( sortDirection, colIndex ) => {
             if ( colIndex === clickedColumnIndex ) {
-                return sortDirection === "ascending" ? newColumnSortStatus[ colIndex ] = "descending" :
-                       newColumnSortStatus[ colIndex ] = "ascending";
+                return sortDirection === "ascending" ? newColumnSortStatusValue[ colIndex ] = "descending" :
+                       newColumnSortStatusValue[ colIndex ] = "ascending";
             } else {
-                return newColumnSortStatus[ colIndex ] = "undefined";
+                return newColumnSortStatusValue[ colIndex ] = "undefined";
             }
         } )
-
-        const order = newColumnSortStatus[ clickedColumnIndex ];
-        let sortedTranslationData = this.state.translationData.slice();
-        sortedTranslationData.sort( ( row1, row2 ) => {
-
-            let word1 = row1[ sortElementIndex ];
-            let word2 = row2[ sortElementIndex ];
-
-            if ( !word1 ) {
-                return 1;
-            } else if ( !word2 ) {
-                return -1;
-            } else {
-
-                const collator = new Intl.Collator();
-
-                if ( order === "ascending" ) {
-                    return collator.compare( word1, word2 );
-                } else {
-                    return collator.compare( word2, word1 );
-                }
-            }
-
-        } );
 
         this.setState( {
-            translationData: sortedTranslationData,
-            columnSortStatus: newColumnSortStatus
+            columnSortStatus: {
+                currentSortedColumnIndex: clickedColumnIndex,
+                sortStatus: newColumnSortStatusValue
+            }
         } )
+    }
+
+    sortColumn() {
+
+        let sortedTranslationData = this.props.data.slice();
+        const sortedColumnIndex = this.state.columnSortStatus.currentSortedColumnIndex;
+
+        // check if a column needs to be sorted
+        if ( sortedColumnIndex >= 0 ) {
+            // headers in the table are in the format: [ chinese + pinyin, english ... ]
+            // translationData contains array in the format: [ chinese, pinyin, english ... ]
+            const dataIndex = sortedColumnIndex + 1;
+
+            const order = this.state.columnSortStatus.sortStatus[ sortedColumnIndex ];
+
+            sortedTranslationData.sort( ( row1, row2 ) => {
+
+                let word1 = row1[ dataIndex ];
+                let word2 = row2[ dataIndex ];
+
+                if ( !word1 ) {
+                    return 1;
+                } else if ( !word2 ) {
+                    return -1;
+                } else {
+
+                    const collator = new Intl.Collator();
+
+                    if ( order === "ascending" ) {
+                        return collator.compare( word1, word2 );
+                    } else {
+                        return collator.compare( word2, word1 );
+                    }
+                }
+
+            } );
+        }
+
+        return sortedTranslationData;
     }
 
     render() {
