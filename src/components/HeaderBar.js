021fb5741fc92dcaacd4653dfb3b8b524fa77e2b
EileenSiqi <lsq0276665696@gmail.com>
2020-09-25 15:26:02 +1200

    [CMLG-019] implement CSS and responsiveness
    Issue:
    The CSS need to redesign.
    
    Solution: 
    Add some CSS code to HeaderBar.css
    
    Risk:
    The initial state need to be changed.
    
    Reviewed by:
    Kevin, Emily, Yujia, Annie, Dave

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,32 +1,58 @@
 import React from "react";
+import "./css/HeaderBar.css";
 import { Link } from "react-router-dom";
 
 class HeaderBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            BarOpen : true
+        }
+        this.HandleBarToggleClick = this.HandleBarToggleClick.bind(this)
+    }
+    HandleBarToggleClick(){
+        this.setState( (prevState) => {
+            return {BarOpen: !prevState.BarOpen}
+        } )
+    }
+
     render() {
+        let values;
+        const items = (
+            <div>
+                <Link to="/">
+                    <li>
+                        <i className="fas fa-home">Home</i>
+                    </li>
+                </Link>
+                <Link to="/translations">
+                    <li>
+                        <i className="fas fa-search">Search</i>
+                    </li>
+                </Link>
+                <Link to="/about">
+                    <li>
+                        <i className="fas fa-info-circle">About</i>
+                    </li>
+                </Link>
+            </div>
+        );
+        if(this.state.BarOpen){
+            values = items;
+        }
         return (
-             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <Link to="/">
-                        <li className="nav-item">
-                            <i className="fas fa-home">Home</i>
-                        </li>
-                    </Link>
-
-
-                    <Link to="/translations">
-                        <li className="nav-item">
-                            <i className="fas fa-search">Search</i>
-                        </li>
-                    </Link>
-
-                    <Link to="/about">
-                        <li className="nav-item">
-                            <i className="fas fa-info-circle">About</i>
-                        </li>
-                    </Link>
-                    <li className="nav-item">
+            <nav>
+             {/*<nav className="navbar navbar-expand-lg navbar-light bg-light">*/}
+             <ul>
+                {/*<ul className="navbar-nav mr-auto mt-2 mt-lg-0">*/}
+                {/* {items}*/}
+                    <li id="bar" onClick={this.HandleBarToggleClick}>
+                        <i className="fas fa-bars"/>
+                    </li>
+                    <li id="darkMode">
                         <i className="fas fa-moon"/>
                     </li>
+                 {values}
                 </ul>
             </nav>
         );
