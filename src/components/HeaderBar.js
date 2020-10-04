0d3f0a0e5c7951cdc6ea65bd56e3119ee113a5a2
EileenSiqi <59998744+EileenSiqi@users.noreply.github.com>
2020-10-04 15:30:22 +1300

    [CMLG-019] Add CSS and responsiveness to the headerbar (#43)
    Issue:
    CSS and responsiveness design for the header bar need to be updated
    
    Solution: 
    Use state to toggle between different designs according to the window size.
    
    Risk:
    Unknown
    
    Reviewed by:
    Kevin, Emily, Yujia, Annie, Dave

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,32 +1,76 @@
 import React from "react";
+import "./css/HeaderBar.css";
 import { Link } from "react-router-dom";
 
 class HeaderBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            BarOpen : window.innerWidth > 600
+        }
+        this.handleBarToggleClick = this.handleBarToggleClick.bind( this )
+        this.updateBarOpen = this.updateBarOpen.bind( this )
+    }
+
+    handleBarToggleClick() {
+        this.setState( ( prevState ) => {
+            return { BarOpen: !prevState.BarOpen }
+        } )
+    }
+
+    updateBarOpen() {
+        if ( window.innerWidth > 600 ) {
+            this.setState( { BarOpen: true } );
+        } else {
+            this.setState( { BarOpen: false } );
+        }
+    }
+
+    // Add event listener
+    componentDidMount() {
+        this.updateBarOpen();
+        window.addEventListener( "resize", this.updateBarOpen);
+    }
+
+    //Remove event listener
+    componentWillUnmount() {
+        window.removeEventListener( "resize", this.updateBarOpen );
+    }
+
     render() {
+        let values;
+        const items = (
+            <div>
+                <Link to="/">
+                    <li>
+                        <i className="fas fa-home">&nbsp;Home</i>
+                    </li>
+                </Link>
+                <Link to="/translations">
+                    <li>
+                        <i className="fas fa-search">&nbsp;Search</i>
+                    </li>
+                </Link>
+                <Link to="/about">
+                    <li>
+                        <i className="fas fa-info-circle">&nbsp;About</i>
+                    </li>
+                </Link>
+            </div>
+        );
+        if( this.state.BarOpen ) {
+            values = items;
+        }
         return (
-             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <Link to="/">
-                        <li className="nav-item">
-                            <i className="fas fa-home">Home</i>
-                        </li>
-                    </Link>
-
-
-                    <Link to="/translations">
-                        <li className="nav-item">
-                            <i className="fas fa-search">Search</i>
-                        </li>
-                    </Link>
-
-                    <Link to="/about">
-                        <li className="nav-item">
-                            <i className="fas fa-info-circle">About</i>
-                        </li>
-                    </Link>
-                    <li className="nav-item">
+            <nav>
+                <ul>
+                    <li id="bar" onClick={ this.handleBarToggleClick }>
+                        <i className="fas fa-bars"/>
+                    </li>
+                    <li id="darkMode">
                         <i className="fas fa-moon"/>
                     </li>
+                    {values}
                 </ul>
             </nav>
         );
