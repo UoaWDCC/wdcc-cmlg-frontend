20812f52e1bc391e9ce00e54e659fb5a56a75100
Kevin Ge <gjw1047340859@gmail.com>
2020-10-23 15:44:38 +1300

    for merger

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -34,10 +34,10 @@ class SearchBar extends React.Component{
     // call the changeWord function in the SearchPage class to change its state
     render() {
         return(
-            <div className='search'> 
+            <div className= 'search ' > 
                 <input className='bar' type="text" placeholder={ this.state.placeholderText } 
                        onChange = { ( e ) => this.props.data.changeWord( e.target.value ) }/>
-                <SearchIcon className='search-icon'/>
+                <SearchIcon className={ this.props.darkMode ? 'search-icon dark-mode' : 'search-icon' }/>
             </div>
         )
     }
