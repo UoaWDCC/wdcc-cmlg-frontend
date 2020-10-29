6452bf4e8573ef8ed8bd4c43fa3d7a93bf61a352
Kevin Ge <gjw1047340859@gmail.com>
2020-10-29 23:49:01 +1300

    Dark mode for welcome page and about us page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./css/WelcomePage.css";
 import { Link } from "react-router-dom";
-import flagsImage from "./background4.png";
+import flagsImageDay from "./background2.png";
+import flagsImageNight from "./background3.png";
 
 class WelcomePage extends React.Component {
     render() {
         return (
             <div className= { this.props.darkMode ?  "welcomePage dark-mode" : "welcomePage" }>
-                <img id="background-img" src={flagsImage} alt="Flags background" />
+                <img id="background-img" src={ this.props.darkMode ? flagsImageDay : flagsImageNight } alt="Flags background" />
                 <Link to="/translations">
                     <button type="button" className={ this.props.darkMode? "btn btn-outline-dark btn-dark-mode " : "btn btn-outline-dark" }>Start searching</button>
                 </Link>
