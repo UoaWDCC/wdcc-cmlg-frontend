5dab2e5b59e30d70d1d863c789efa8775f281106
Kevin Ge <gjw1047340859@gmail.com>
2020-10-31 11:16:52 +1300

    welcome page image and format

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./css/WelcomePage.css";
 import { Link } from "react-router-dom";
-import flagsImageNight from "./background2.png";
-import flagsImageDay from "./background3.png";
+import flagsImageNight from "./flagsImageNight.png";
+import flagsImageDay from "./flagsImageDay.png";
 
 class WelcomePage extends React.Component {
     render() {
         return (
-            <div className= { this.props.darkMode ?  "welcomePage dark-mode" : "welcomePage" }>
+            <div className= { ` welcomePage ${ this.props.darkMode ? "dark-mode" : "" } `}>
                 <img id="background-img" src={ this.props.darkMode ? flagsImageNight : flagsImageDay } alt="Flags background" />
                 <Link to="/translations">
-                    <button type="button" className={ this.props.darkMode? "btn btn-outline-dark btn-dark-mode " : "btn btn-outline-dark" }>Start searching</button>
+                    <button type="button" className={ ` btn btn-outline-dark ${ this.props.darkMode? "btn-dark-mode" : "" } ` }>Start searching</button>
                 </Link>
             </div>
 
