098451df35665f9306d5d9382c8cdaf04aeb7e25
Kevin Ge <gjw1047340859@gmail.com>
2020-10-30 16:12:51 +1300

    Dark mode for toggle button, using $ to dynamically add class

--- src/components/RowsPerPageToggleButton.js
+++ src/components/RowsPerPageToggleButton.js
@@ -7,11 +7,12 @@ export function RowsPerPageToggleButton( props ) {
 	return (
 
 		<div className="btn-group btn-group-toggle" data-toggle="buttons">
-			<label className= { props.darkMode ? "btn btn-light toggle-button rounded-0 active dark-mode-active" : "btn btn-light toggle-button rounded-0 active" }>
+			<label className= { `btn btn-light toggle-button rounded-0 active ${ props.darkMode ? "dark-mode-active" : "" } `}>
 				<input type="radio" name= "options " id="TenRows" value={ "10" } defaultChecked
 					   onClick={ event => props.onButtonClicked( event.target.value ) }/> 10 rows
 			</label>
-			<label className= { props.darkMode ? "btn btn-light toggle-button rounded-0 dark-mode" :  "btn btn-light toggle-button rounded-0" }>
+			<label className= { `btn btn-light toggle-button rounded-0 ${ props.darkMode ? "dark-mode" : "" } `}>
+
 				<input type="radio" name="options" id="all" value={ "all" }
 					   onClick={ event => props.onButtonClicked( event.target.value ) }/> all
 			</label>
