ddabbb84719faac7d260a6c1c5edebb5c747f8f9
Kevin Ge <gjw1047340859@gmail.com>
2021-03-11 15:07:00 +1300

    Solve the toggle button dark mode bug by having a state

--- src/components/RowsPerPageToggleButton.js
+++ src/components/RowsPerPageToggleButton.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../css/RowsPerPageToggleButton.css"
 
 
 export function RowsPerPageToggleButton( props ) {
 
+	const [row10active, setRow10Active] = useState(true)
+
 	return (
 
 		<div className="btn-group btn-group-toggle" data-toggle="buttons">
-			<label className= { `btn btn-light toggle-button rounded-0 active ${ props.darkMode ? "dark-mode-active" : "" } `}>
+			<label className= { `btn btn-light toggle-button rounded-0 ${ props.darkMode ? "dark-mode" : "" } ${ row10active ? "active" : "" } `}>
 				<input type="radio" name= "options " id="TenRows" value={ "10" } defaultChecked
-					   onClick={ event => props.onButtonClicked( event.target.value ) }/> 10 rows
+					   onClick={ event => { props.onButtonClicked( event.target.value ); setRow10Active(true) }  }/> 10 rows
 			</label>
-			<label className= { `btn btn-light toggle-button rounded-0 ${ props.darkMode ? "dark-mode" : "" } `}>
+			<label className= { `btn btn-light toggle-button rounded-0 ${ props.darkMode ? "dark-mode" : "" } ${ row10active ? "" : "active" } `}>
 
 				<input type="radio" name="options" id="all" value={ "all" }
-					   onClick={ event => props.onButtonClicked( event.target.value ) }/> all
+					   onClick={ event => { props.onButtonClicked( event.target.value ); setRow10Active(false) } }/> all
 			</label>
 		</div>
 	);
