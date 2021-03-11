7bd08b03085bfc1b2abe98bb1a84ffc320f957e4
Annie Chau <annithinggoes@gmail.com>
2021-03-11 15:41:42 +1300

    [CMLG-024] Update welcome page + refactor
    Issue:
    Client wants new design for front page.
    
    Solution:
    Removed image and new title.
    Removed some unused css styling.
    Ensure responsiveness.
    
    Risk:--
    
    Reviewed by:
    Emily, Dave, Eileen, Kevin, Yujia

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "../css/WelcomePage.css";
 import { Link } from "react-router-dom";
-import flagsImageNight from "../resource/flagsImageNight.png";
-import flagsImageDay from "../resource/flagsImageDay.png";
 
 class WelcomePage extends React.Component {
     render() {
         return (
             <div className={ ` welcomePage ${ this.props.darkMode ? "dark-mode" : "" } ` }>
-                <img id="background-img" src={ this.props.darkMode ? flagsImageNight : flagsImageDay }
-                     alt="Flags background" />
+                <h id="title-txt">COVID-19 Multilingual Glossary</h>
+                <br />
                 <Link to="/translations">
                     <button type="button" className={ ` btn btn-outline-dark 
                     ${ this.props.darkMode? "btn-dark-mode" : "" } ` }>Start searching</button>
