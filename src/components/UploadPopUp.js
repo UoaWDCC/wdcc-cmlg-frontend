5aa66baeb5385fa39e4964b2f61f9698dc57a436
Annie Chau <annithinggoes@gmail.com>
2021-05-13 13:28:38 +1200

    [CMLG-29] Add file selection button
    Issue:
    Need button to select file
    
    Solution:
    Include file input in form tag
    Included file onChange event handling
    
    Risk: -
    
    Reviewed by: Dave, Kevin, Eileen, Hayoon

--- src/components/UploadPopUp.js
+++ src/components/UploadPopUp.js
@@ -1,30 +1,45 @@
-import React from 'react';
-import "../css/UploadPopUp.css";
+import React, { useState } from 'react';
 
 function UploadPopUp() {
+
+    const [file, setFile] = useState();
+
+    function onChangeHandler(e) {
+         setFile(e.target.files[0])
+    }
+
+    function handleUploadSubmit(e) {
+        console.log("file to submit",file)
+        // Validate the file => correct extension/ headers
+        
+        // Make fetch/axios call to backend to upload the file 
+        // https://cmlgbackend.wdcc.co.nz/api/uploadfile
+    }
 
     return (
         <div className='UploadPopUp '>
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                 Launch demo modal
             </button>
-            <div class="modal" tabindex="-1" role="dialog" id="exampleModal">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Upload Translations</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <div className="modal" tabIndex="-1" role="dialog" id="exampleModal">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Upload Test</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             <p>Accepted format: *.xlsx</p>
-                            <form>
-                                <input></input>
+                            {/* Testing out the form submission */}
+                            {/* <form id='fileForm' enctype="multipart/form-data" method="post" action='https://cmlgbackend.wdcc.co.nz/api/uploadfile'> */}
+                            <form id='fileForm' action='#'>
+                                <input type="file" accept=".xlsx" onChange={onChangeHandler}/>                                
                             </form>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                        <div className="modal-footer">
+                            <button type="submit" form='fileForm' className="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
