4261939ad6dbccafd663869315a7264bd867b88b
Emily Yang <zyan225@aucklanduni.ac.nz>
2021-10-06 13:40:29 +1300

    [CMLG-044] reformat login page to use 4 space indentation

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,113 +3,112 @@ import AuthContext from "../context/AuthContext";
 import styles from "../css/LoginPage.module.css";
 
 const LoginForm = (props) => {
-  const { storeToken } = useContext(AuthContext);
+    const { storeToken } = useContext(AuthContext);
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [success, setSuccess] = useState(false);
-  const [error, setError] = useState("");
-  const [body, setBody] = useState("");
-  const [firstRender, setFirstRender] = useState(true);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState("");
+    const [body, setBody] = useState("");
+    const [firstRender, setFirstRender] = useState(true);
 
-  useEffect(() => {
-    if (!firstRender) {
-      let url = "https://cmlgbackend.wdcc.co.nz/api/login";
-      fetch(url, {
-        method: "POST",
-        body: {
-          username: username,
-          password: password,
-        },
-      })
-        .then((response) => response.json())
-        .then((result) => {
-          // handle token
-          console.log(result);
-          storeToken(result);
-          setSuccess(true);
-        })
-        .catch((error) => {
-          setError(error);
-          setSuccess(false);
-        });
-    } else {
-      setFirstRender(false);
+    useEffect(() => {
+        if (!firstRender) {
+            let url = "https://cmlgbackend.wdcc.co.nz/api/login";
+            fetch(url, {
+                method: "POST",
+                body: {
+                    username: username,
+                    password: password,
+                },
+            })
+                .then((response) => response.json())
+                .then((result) => {
+                    // handle token
+                    console.log(result);
+                    storeToken(result);
+                    setSuccess(true);
+                })
+                .catch((error) => {
+                    setError(error);
+                    setSuccess(false);
+                });
+        } else {
+            setFirstRender(false);
+        }
+    }, [body]);
+
+    function handleSubmit() {
+        const newBody = {
+            username: username,
+            password: password,
+        };
+
+        setBody(newBody);
     }
-  }, [body]);
 
-  function handleSubmit() {
-    const newBody = {
-      username: username,
-      password: password,
-    };
-
-    setBody(newBody);
-  }
-
-  return (
-    <div
-      className={` ${styles.container} ${props.darkMode ? "dark-mode" : ""} `}
-    >
-      {success ? (
-        <div>
-          Logined successfully, please go to setting to upload documents.
+    return (
+        <div
+            className={` ${styles.container} ${props.darkMode ? "dark-mode" : ""} `}
+        >
+            {success ? (
+                <div>
+                    Logined successfully, please go to setting to upload documents.
+                </div>
+            ) : (
+                <>
+                    <div className={styles.header}>Login</div>
+                    <div className={styles.form}>
+                        <div className={styles["form-group"]}>
+                            <label htmlFor="username">Username</label>
+                            <input
+                                value={username}
+                                className={styles["login-input"]}
+                                type="text"
+                                name="username"
+                                placeholder="username"
+                                onChange={(e) => {
+                                    setUsername(e.target.value);
+                                }}
+                            />
+                        </div>
+                        <div className={styles["form-group"]}>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                value={password}
+                                className={styles["login-input"]}
+                                type="password"
+                                name="password"
+                                placeholder="password"
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                }}
+                            />
+                        </div>
+                    </div>
+                    {error ? (
+                        <div
+                            className={
+                                props.darkMode ? styles.textDangerDark : styles.textDangerLight
+                            }
+                        >
+                            {error}
+                        </div>
+                    ) : null}
+                    <div className={styles.footer}>
+                        <button
+                            type="button"
+                            className={` btn btn-outline-dark ${props.darkMode ? styles["btn-dark-mode"] : ""
+                                } }`}
+                            onClick={handleSubmit}
+                        >
+                            Login
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
-      ) : (
-        <>
-          <div className={styles.header}>Login</div>
-          <div className={styles.form}>
-            <div className={styles["form-group"]}>
-              <label htmlFor="username">Username</label>
-              <input
-                value={username}
-                className={styles["login-input"]}
-                type="text"
-                name="username"
-                placeholder="username"
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-              />
-            </div>
-            <div className={styles["form-group"]}>
-              <label htmlFor="password">Password</label>
-              <input
-                value={password}
-                className={styles["login-input"]}
-                type="password"
-                name="password"
-                placeholder="password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-            </div>
-          </div>
-          {error ? (
-            <div
-              className={
-                props.darkMode ? styles.textDangerDark : styles.textDangerLight
-              }
-            >
-              {error}
-            </div>
-          ) : null}
-          <div className={styles.footer}>
-            <button
-              type="button"
-              className={` btn btn-outline-dark ${
-                props.darkMode ? styles["btn-dark-mode"] : ""
-              } }`}
-              onClick={handleSubmit}
-            >
-              Login
-            </button>
-          </div>
-        </>
-      )}
-    </div>
-  );
+    );
 };
 
 export default LoginForm;
